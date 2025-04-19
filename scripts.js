0c7068de65b688e6cdb44e36c5ba0f6257bd749e
klumzy33 <92058482+klumzy33@users.noreply.github.com>
2025-04-19 15:40:20 -0400

    email swap

--- scripts.js
+++ scripts.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
 const timelineData = [
     {
-        date: 'Sept 2022 - Dec 2023',
+        date: 'Dec 2021 - Dec 2023',
         title: 'Test Technician 2',
         description: 'Collaborated with team members on calculating passing algorithms for individual systems before they are sold to clients overseas.\n\nAlgorithms include the results of the samples that were used, passing the hvf, LHPC1 svf, LHPC2 svf criteria.\nIf the results fail any one of these 3 criterias, certain troubleshooting steps need to be administered.'
     },
     {
-        date: 'Oct 2016 - Sept 2017',
+        date: 'Sep 2016 - Sept 2017',
         title: 'Software Developer',
         description: 'Actively engaging in SCRUM meetings to discuss weekly objectives and ensuring timely completion of tasks by the end of each week' + '\nUpdating the front end of websites in .NET connecting through C#/HTML and occasional updates to the tables in SQL Server.\nClients can now see updated information on their websites on the front end of the websites.'
     }
